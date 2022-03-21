d666ad4c6598468235df3f61e66d496a72f253f9
dias-steve <stevediaspro@gmail.com>
2022-03-21 18:32:18 +0100

    setting paraméter status and per_page on URI API

--- src/redux/Products/products.helpers.js
+++ src/redux/Products/products.helpers.js
@@ -3,7 +3,7 @@
 export const handleFetchProducts = () => {
     return new Promise ((resolve, reject) => {
         ////"https://woocoreactplugin.local/wp-json/wc/v2/products/?consumer_key=ck_bc8bc21913c80284ef1bc3ff743dbf9c88b59622&consumer_secret=cs_00bcac954138822adfa82502bbab58b4c9d80c7f")//
-      fetch("https://woocoreactplugin.local/wp-json/wc/v2/products/?consumer_key="+process.env.REACT_APP_WOO_PUBLIC_API+"&consumer_secret="+process.env.REACT_APP_WOO_SECRET_API)
+      fetch("https://woocoreactplugin.local/wp-json/wc/v2/products/?status=publish&per_page=100&consumer_key="+process.env.REACT_APP_WOO_PUBLIC_API+"&consumer_secret="+process.env.REACT_APP_WOO_SECRET_API)
         .then( r => {
            resolve (r.json());
         })
